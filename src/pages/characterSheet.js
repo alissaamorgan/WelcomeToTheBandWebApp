ecda317f49e18372c81b940f1aad9c6dfe8cf4b3
Alissa Morgan <alissamorgan2013@gmail.com>
2026-07-20 23:57:53 -0600

    character sheet css

--- src/pages/characterSheet.js
+++ src/pages/characterSheet.js
@@ -1,29 +1,47 @@
 import {React, useState} from "react";
 import './pages.css';
+import characterSelection from '../assets/CharacterSelection.png'
+import longRestButton from '../assets/LongRestButton.png'
+import phone from '../assets/Phone.png'
+import exampleSheet from '../assets/ExampleSheet.png'
 
 const CharacterSheet = () => {
       const [count, setCount] = useState(0)
 
     return (
         <div>
-            <h1>CharacterSheet</h1>
-            <button
-                type="button"
-                className="counter"
-                color="green"
-                onClick={() => setCount((count) => count + 1)}
-                >
-                    +
-            </button>
-            Count is {count}
-            <button
-                type="button"
-                className="counter"
-                color = "red"
-                onClick={() => setCount((count) => count - 1)}
-                >
-                    -
-            </button>
+        <div class="characterSheetHeader">
+            <img class = "headerCharacterSelectionButton" src={characterSelection} alt="CharacterSelection" onClick={() => window.location = '/characterSelection'}></img>
+            <img class = "headerLongRestButton" src={longRestButton} alt="LongRestButton" onClick={() => alert("Long Rest Taken!")}></img>
+        </div>
+        <div class="characterSheetPage">
+            <div class="characterSheetPhone">
+                <img class = "phone" src={phone} alt="Phone"></img>
+            </div>
+            <div class="characterSheetSheet">
+                <div class="characterSheetModStats">
+                    <div class="characterSheetHP, column">
+                        <h3>Current Hit Points</h3>
+                        <button type="button" className="hpCounterUp" onClick={() => setCount((count) => count + 1)}>
+                                +
+                        </button>
+                        <h4>{count}</h4>
+                        <button type="button" className="hpCounterDown" onClick={() => setCount((count) => count - 1)}>
+                                -
+                        </button>
+                    </div>
+                    <div class="column">
+                            2
+                    </div>
+                    <div class="column">
+                            3
+                    </div>
+                </div>
+                <div class="characterSheet">
+                    <img class = "sheet" src={exampleSheet} alt="Sheet"></img>
+                </div>
+            </div>
+        </div>
         </div>
     );
 };
